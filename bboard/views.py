7193381ad9f41de987ec584f1d15c5a83a973242
Dmitriy Veshniakov <dv@paypro.ru>
2020-03-15 21:01:58 +0700

    * завершили тестовый сайт

--- bboard/views.py
+++ bboard/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 from .models import Bb
 from .models import Rubric
+
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .forms import BbForm
 # Create your views here.
 
 def index(request):
@@ -17,3 +22,12 @@ def by_rubric(request, rubric_id):
     context={'bbs':bbs, 'rubrics':rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
 
+class BbCreateView(CreateView):
+    template_name='bboard/create.html'
+    form_class=BbForm
+    success_url=reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['rubrics']=Rubric.objects.all()
+        return context
